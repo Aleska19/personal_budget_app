8c763cf2c9799dc07c57b35fb609d94cc9ef247b
Aleska <cegarraaleska91@gmail.com>
2024-09-24 23:41:21 -0300

    mi primer simulador de usuario

---
+++ main.js
@@ -0,0 +1,59 @@
+const ingreso = [];
+const gastos = [];
+
+
+function ingresosMensuales(){
+    const ingresoMensual = parseFloat(prompt("Cual es tu ingreso mensual? "));
+    console.log("Tu ingreso mensual es de: " +  ingresoMensual);
+
+    ingreso.push({ingresoMensual})
+}
+
+
+function gastosMensuales(){
+    let continuarAgregando = true;
+    while (continuarAgregando){
+        const descripcion = prompt("Me indicas la descripcion? ");
+        const monto = parseFloat(prompt("Me indicas  monto de los gastos mensuales? "));
+
+        gastos.push({descripcion: descripcion, monto: monto});
+
+        continuarAgregando = confirm("Quieres Continuar agregando? ");
+    }
+}
+
+function calcularTotalGastos(){
+    let total = 0;
+    gastos.forEach(gasto => {
+        total += gasto.monto;
+        console.log(`El Total de gastos es: ` + total + ' pesos');
+    });
+}
+
+calcularTotalGastos();
+
+function mostrarGastos(){
+    console.log('Resumen de gastos:' )
+    gastos.forEach(gasto => {
+        console.log(`${gasto.descripcion}: ${gasto.monto} pesos`);
+        
+    });
+}
+
+mostrarGastos()
+
+let saldo = ingreso - gastos;
+function saldoTotal(){
+    console.log("Te queda un saldo de: " + "" +  saldo);
+
+    if (ingreso > gastos){
+        console.log("Vas muy bien, puedes ahorrar lo que te queda")
+    } else{
+        console.log("debes intentar reducir un poco tus gastos")
+    }
+}
+
+
+
+
+
---
+++ main.md
@@ -0,0 +1,7 @@
+Simulador de presupuesto personal 
+
+Un simulador donde los usuarios pueden ingresar sus ingresos, gastos y objetivos de ahorro. Con cada iteracion, el simulador ajusta los valores para mostrar como podrian alcanzar sus metas de ahorro o reducir gastos a lo largo del tiempo. 
+
+Parametros iterativos: Ingresos Mensuales, gastos variables (alquiler, comida, ocio), objetivos de ahorro, inflacion. 
+
+Objetivo: Ayudar a visualizar como pequeños cambios en los habitos de gasto pueden impactar en las finanzas a largo plazo.
